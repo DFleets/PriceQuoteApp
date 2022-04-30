0fbfe702cb852895e1997274ec9fbcac168d7731
Ravend20 <Fleetsd@yahoo.com>
2022-04-30 06:53:27 -0400

    Add project files.

---
+++ PriceQuoteApp/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PriceQuoteApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriceQuoteApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(PriceQuoteAppModel model)
+        {
+            if (ModelState.IsValid)
+
+            {
+                model.DiscountAmount = model.SubTotal * model.DiscountPercent / 100;
+                model.Total = model.SubTotal - model.DiscountAmount;
+
+            }
+
+            return View(model);
+        }
+    }
+}
+
---
+++ PriceQuoteApp/Models/PriceQuoteAppModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PriceQuoteApp.Models
+{
+    public class PriceQuoteAppModel
+    {
+        
+        [Range(0, int.MaxValue)]
+        [Required(ErrorMessage = "Must be a valid number greater than 0")]
+        public decimal? SubTotal { get; set; }
+
+        
+        [Range(0, 100)]
+        [Required(ErrorMessage = "Must be a valid number between 0 to 100")]
+        public decimal? DiscountPercent { get; set; }
+
+        public decimal? DiscountAmount { get; set; }
+
+       
+        public decimal? Total { get; set; }
+
+    }
+}
